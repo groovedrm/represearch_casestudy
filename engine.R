4edfb29510769f3caa30696e9ade620beeb17412
ccocuzzo <ccocuzzo@nephilaadvisors.com>
2017-06-21 21:21:05 -0400

    Adding more to the engine, need to figure out the piece for the economic damage but getting close

--- engine.R
+++ engine.R
@@ -1,5 +1,53 @@
 setwd("/users/chris/development/r/represearch_casestudy")
 
+# Loading Up Libraries/Packages
+is.installed <- function(mypkg){
+  is.element(mypkg, installed.packages()[,1])
+}
+
+if (!is.installed("data.table")){
+  install.packages("data.table")
+}
+
+if (!is.installed("plyr")){
+  install.packages("plyr")
+}
+
+if (!is.installed("dplyr")){
+  install.packages("dplyr")
+}
+
+if (!is.installed("ggplot2")){
+  install.packages("ggplot2")
+}
+
+library(data.table)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+
+unitConverter <- function(v) {
+  v <- as.character(v)
+  if (v %in% c("h","H")) {
+    return(2)
+  }
+  else if (v %in% c("k","K")) {
+    return(3)
+  }
+  else if (v %in% c("m","M")) {
+    return(6)
+  }
+  else if (v %in% c("b","B")) {
+    return(9)
+  }
+  else if (as.numeric(v %in% c(1:9))) {
+    return(as.numeric(v))
+  }
+  else {
+    return(0)
+  }
+}
+
 # If data doesn't already exist, go get it.
 if (!file.exists("repdata-data-StormData.csv")) {
   zipURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
@@ -10,4 +58,30 @@ if (!file.exists("repdata-data-StormData.csv")) {
 # Load Up Data
 sourceDir <- getSrcDirectory(function (d) {d})
 dirForFile <- paste(sourceDir,"/","repdata-data-StormData.csv", sep = "")
-baseData <- fread(dirForFile, sep="auto", stringsAsFactors = TRUE)
+baseData <- read.table(dirForFile, header=TRUE, sep=",", stringsAsFactors = TRUE)
+distilledData <- baseData[, c("EVTYPE", "FATALITIES", "INJURIES", "PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP")]
+# distilledData$PROPDMGEXP <- unitConverter(distilledData$PROPDMGEXP) 
+# distilledData$CROPDMGEXP <- unitConverter(distilledData$CROPDMGEXP)
+# Above, don't need apply() since the variables are just lists.
+
+# Aggregate and then reorder to get the top events for fatalities and injuries
+healthBaseTable <- ddply(distilledData, .(EVTYPE), summarize, fatalities = sum(FATALITIES), injuries = sum(INJURIES))
+injuriesByEvent <- healthBaseTable[order(-healthBaseTable$injuries), ]
+fatalitiesByEvent <- healthBaseTable[order(-healthBaseTable$fatalities), ]
+topInjuriesByEvent <- injuriesByEvent[1:10, ]
+topFatalitiesByEvent <- fatalitiesByEvent[1:10, ]
+
+# Make some plots of these two.
+par(mfrow=c(2,1))
+p1 <- ggplot(data = topInjuriesByEvent, aes(x = reorder(EVTYPE, -injuries), y = injuries)) +
+              geom_bar(fill="Blue", stat="identity") +
+              coord_flip() +
+              theme_minimal()
+
+p2 <- ggplot(data = topFatalitiesByEvent, aes(x = reorder(EVTYPE, -fatalities), y = fatalities)) +
+              geom_bar(fill="Red", stat="identity") +
+              coord_flip() +
+              theme_minimal()
+
+
+
