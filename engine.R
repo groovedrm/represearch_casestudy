91074c5396160190bcee15e6fa24ba1cafdaed5f
ccocuzzo <ccocuzzo@nephilaadvisors.com>
2017-06-22 07:30:44 -0400

    Engine working on all data. Next, will address laying out the plots and labeling them

--- engine.R
+++ engine.R
@@ -60,9 +60,10 @@ sourceDir <- getSrcDirectory(function (d) {d})
 dirForFile <- paste(sourceDir,"/","repdata-data-StormData.csv", sep = "")
 baseData <- read.table(dirForFile, header=TRUE, sep=",", stringsAsFactors = TRUE)
 distilledData <- baseData[, c("EVTYPE", "FATALITIES", "INJURIES", "PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP")]
-# distilledData$PROPDMGEXP <- unitConverter(distilledData$PROPDMGEXP) 
-# distilledData$CROPDMGEXP <- unitConverter(distilledData$CROPDMGEXP)
-# Above, don't need apply() since the variables are just lists.
+propUnit <- sapply(distilledData$PROPDMGEXP, FUN = unitConverter)
+distilledData$propDamage <- distilledData$PROPDMG * (10 ** propUnit)
+cropUnit <- sapply(distilledData$CROPDMGEXP, FUN = unitConverter)
+distilledData$cropDamage <- distilledData$CROPDMG * (10 ** cropUnit)
 
 # Aggregate and then reorder to get the top events for fatalities and injuries
 healthBaseTable <- ddply(distilledData, .(EVTYPE), summarize, fatalities = sum(FATALITIES), injuries = sum(INJURIES))
@@ -71,7 +72,7 @@ fatalitiesByEvent <- healthBaseTable[order(-healthBaseTable$fatalities), ]
 topInjuriesByEvent <- injuriesByEvent[1:10, ]
 topFatalitiesByEvent <- fatalitiesByEvent[1:10, ]
 
-# Make some plots of these two.
+# Plot the health-based consequences in using the plot code below.
 par(mfrow=c(2,1))
 p1 <- ggplot(data = topInjuriesByEvent, aes(x = reorder(EVTYPE, -injuries), y = injuries)) +
               geom_bar(fill="Blue", stat="identity") +
@@ -82,6 +83,32 @@ p2 <- ggplot(data = topFatalitiesByEvent, aes(x = reorder(EVTYPE, -fatalities), 
               geom_bar(fill="Red", stat="identity") +
               coord_flip() +
               theme_minimal()
+
+# Ok, now measure the economic losses using the damage estimates. 
+economicBaseTable <- ddply(distilledData, .(EVTYPE), summarize, propDamage = sum(propDamage), cropDamage = sum(cropDamage))
+propDmgByEvent <- economicBaseTable[order(-economicBaseTable$propDamage), ]
+topPropDmg <- propDmgByEvent[1:10, ]
+names(topPropDmg) <- c("EventType","PropertyDamage","CropDamage")
+cropDmgByEvent <- economicBaseTable[order(-economicBaseTable$cropDamage), ]
+topCropDmg <- cropDmgByEvent[1:10, ]
+names(topCropDmg) <- c("EventType","PropertyDamage","CropDamage")
+
+# Now, plot the economics consequences
+png(filename = "plot_panel_2.png", width = 480, height = 480)
+par(mfrow=c(2,1))
+#p3
+ggplot(topPropDmg, aes(x = reorder(EventType, -PropertyDamage), y = PropertyDamage)) +
+              geom_bar(fill = "Blue", stat="identity") +
+              coord_flip() +
+              theme_minimal()
+#p4
+ggplot(topCropDmg, aes(x = reorder(EventType, -CropDamage), y = CropDamage)) +
+              geom_bar(fill = "Red", stat="identity") + 
+              coord_flip() +
+              theme_minimal()
+
+dev.off()
+ 
 
 
 
